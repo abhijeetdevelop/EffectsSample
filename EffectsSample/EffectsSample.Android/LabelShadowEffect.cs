ebbe69a7e56d797936b538764195a2049ae47852
Abhijeet <abhijeet.abhijeet@soprasteria.com>
2019-06-30 20:42:44 +0530

    Add project files.

---
+++ EffectsSample/EffectsSample.Android/LabelShadowEffect.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using EffectsSample.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(LabelShadowEffect), "LabelShadowEffect")]
+namespace EffectsSample.Droid
+{
+    class LabelShadowEffect : PlatformEffect
+    {
+        Android.Widget.TextView control;
+        Android.Graphics.Color color;
+        float radius, distanceX, distanceY;
+        protected override void OnAttached()
+        {
+            try
+            {
+                control = Control as Android.Widget.TextView;
+                UpdateRadius();
+                UpdateColor();
+                UpdateOffset();
+                UpdateControl();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == ShadowEffect.RadiusProperty.PropertyName)
+            {
+                UpdateRadius();
+                UpdateControl();
+            }
+            else if (args.PropertyName == ShadowEffect.ColorProperty.PropertyName)
+            {
+                UpdateColor();
+                UpdateControl();
+            }
+            else if (args.PropertyName == ShadowEffect.DistanceXProperty.PropertyName ||
+                     args.PropertyName == ShadowEffect.DistanceYProperty.PropertyName)
+            {
+                UpdateOffset();
+                UpdateControl();
+            }
+        }
+
+        protected override void OnDetached()
+        {            
+        }
+        void UpdateControl()
+        {
+            if (control != null)
+            {
+                control.SetShadowLayer(radius, distanceX, distanceY, color);
+            }
+        }
+
+        void UpdateRadius()
+        {
+            radius = (float)ShadowEffect.GetRadius(Element);
+        }
+
+        void UpdateColor()
+        {
+            color = ShadowEffect.GetColor(Element).ToAndroid();
+        }
+
+        void UpdateOffset()
+        {
+            distanceX = (float)ShadowEffect.GetDistanceX(Element);
+            distanceY = (float)ShadowEffect.GetDistanceY(Element);
+        }
+    }
+}
